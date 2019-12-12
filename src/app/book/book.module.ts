04363c11fad455d9d82d952929f95ac2946d36f3
jecanizarez <je.canizarez@uniandes.edu.co>
2019-12-12 17:15:46 -0500

    filtro libros por nombre

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -4,13 +4,14 @@ import { BookListComponent } from './book-list/book-list.component';
 import { BookService } from './book.service';
 import { FormsModule }   from '@angular/forms';
 import { BookDetailComponent } from './book-detail/book-detail.component';
+import { FilterPipe }from './filter.pipe';
 
 @NgModule({
   imports: [
     CommonModule, 
     FormsModule
   ],
-  declarations: [BookListComponent, BookDetailComponent], 
+  declarations: [BookListComponent, BookDetailComponent, FilterPipe], 
     exports: [BookListComponent],
     providers: [BookService]
 })
