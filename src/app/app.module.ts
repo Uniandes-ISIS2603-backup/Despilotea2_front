929e719c699b69488ea7c41d033e5a3a5651840a
Angela Suarez <am.suarezp@uniandes.edu.co>
2019-12-12 08:30:08 -0500

    creacion de carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './app/home/home.component';
+import { AuhtorComponent } from './auhtor/auhtor.component';
+import { BookComponent } from './book/book.component';
+import { EditorialComponent } from './editorial/editorial.component';
+import { OrganizationComponent } from './organization/organization.component';
+import { PrizeComponent } from './prize/prize.component';
+import { ReviewComponent } from './review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AuhtorComponent,
+    BookComponent,
+    EditorialComponent,
+    OrganizationComponent,
+    PrizeComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
