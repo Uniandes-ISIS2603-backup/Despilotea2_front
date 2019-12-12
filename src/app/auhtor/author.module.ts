4a31d66031e7f345d909e4341a2d38981bcab2b0
jecanizarez <je.canizarez@uniandes.edu.co>
2019-12-12 12:36:06 -0500

    author list modificado y su detail

---
+++ src/app/auhtor/author.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthorListComponent } from './author-list/author-list/author-list.component';
+import { AuthorDetailComponent } from './author-detail/author-detail.component';
+
+
+
+@NgModule({
+  declarations: [AuthorListComponent, AuthorDetailComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AuthorModule { }
