164cd699c33d9cd337556ed1124c1859ed8775ad
Dianis Caro <dm.caro@uniandes.edu.co>
2019-12-12 17:28:49 -0500

    fdg

---
+++ src/app/review/rewiew.ts
@@ -0,0 +1,16 @@
+export class Review {
+
+    id:number;
+    
+    /**
+   * Atributo que modela el nombre del cliente.
+   */
+    name: string;
+ 
+    /**
+ * Atributo que modela el apellido del cliente.
+ */
+    description; string;
+ 
+    source: string;
+  }
