c99d47ebde15291da04f9a3d05c097783c058d2f
Dianis Caro <dm.caro@uniandes.edu.co>
2019-12-12 12:27:36 -0500

    conexion a ramas

---
+++ src/app/book/book-list/book-list.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import 'rxjs/add/operator/filter';
+
+
+import {Book} from '../../book/book';
+import {BookService} from '../../book/book.service';
+@Component({
+    selector: 'app-book-list',
+    templateUrl: './book-list.component.html',
+    styleUrls: []
+})
+export class BookListComponent implements OnInit {
+
+    /**
+    * The list of books to display
+    */
+    @Input() books: Book[];
+
+    /**
+    * The component's constructor
+    */
+    constructor(private bookService: BookService, private route: ActivatedRoute) {}
+
+    allbooks: string = 'no';
+    /**
+    * This method retrieves all the books in the Bookstore to show them in the list
+    */
+    getBooks(): void {
+        this.bookService.getBooks()
+            .subscribe(books => {
+                this.books = books;
+            });
+    }
+
+    /**
+    * The method which initializes the component
+    */
+    ngOnInit() {
+        this.getBooks();
+           }
+
+}
