236a5782846325e661b17cac744b065e798570eb
jecanizarez <je.canizarez@uniandes.edu.co>
2019-12-12 17:37:01 -0500

    filtrar autores listos

--- src/app/author/author.module.ts
+++ src/app/author/author.module.ts
@@ -5,16 +5,19 @@ import { AuthorDetailComponent } from './author-detail/author-detail.component';
 import { AuthorService } from './author.service';
 import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { BookModule } from '../book/book.module';
+import { FilterPipe }from './filter.pipe';
+import { FormsModule } from '@angular/forms';
 
 
 
 
 @NgModule({
-  declarations: [AuthorListComponent, AuthorDetailComponent],
+  declarations: [AuthorListComponent, AuthorDetailComponent,FilterPipe],
   imports: [
     CommonModule,
     AppRoutingModule,
-    BookModule
+    BookModule,
+    FormsModule
   ],
   providers: [AuthorService],
   exports: [AuthorListComponent, AuthorDetailComponent ]
