c42eba9ec23d2d4fed80016078dcbdfa06d2a5b4
Angela Suarez <am.suarezp@uniandes.edu.co>
2019-12-12 08:42:42 -0500

    corre

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,14 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+//export const environment = {
+ // production: false
+//};
+
+//Learn more or give us feedback
 export const environment = {
-  production: false
+  production: false,
+  apiURL: 'http://localhost:8080/frontstepbystep-api/'
 };
 
 /*
